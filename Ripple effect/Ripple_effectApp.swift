50c2f3d6f5f2c57faf4b1445f383f4d5e27ba58d
pierrick <pierrickviret@gmail.com>
2025-02-27 10:06:44 +0100

    Initial Commit

---
+++ Ripple effect/Ripple_effectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ripple_effectApp.swift
+//  Ripple effect
+//
+//  Created by pierrick viret on 25/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct Ripple_effectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Ripple effectTests/Ripple_effectTests.swift
@@ -0,0 +1,17 @@
+//
+//  Ripple_effectTests.swift
+//  Ripple effectTests
+//
+//  Created by pierrick viret on 25/02/2025.
+//
+
+import Testing
+@testable import Ripple_effect
+
+struct Ripple_effectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
